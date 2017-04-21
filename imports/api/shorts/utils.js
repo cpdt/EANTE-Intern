c89e29ab4c265a89445626e8c46051f12e671366
cpdt <copodt@gmail.com>
2017-04-21 14:16:29 +1000

    add custom base64 encoders

--- imports/api/shorts/utils.js
+++ imports/api/shorts/utils.js
@@ -1,5 +1,13 @@
 import { Shorts } from './shorts.js';
 
+function btoa(str) {
+    return new Buffer(str).toString('base64');
+}
+
+function atob(b64encoded) {
+    return new Buffer(b64encoded, 'base64').toString();
+}
+
 export function generateUrl() {
     let recent = Shorts.findOne({ creator: 0 }, { sort: { createdAt: -1 } });
     let lastNumber = recent === undefined ? 0 : parseInt(atob(recent.short), 36);
